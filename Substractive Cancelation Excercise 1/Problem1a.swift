7702cab827dc581d1b636610a85919c79367012f
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-01-25 12:43:56 -0600

    Problem 1

---
+++ Substractive Cancelation Excercise 1/Problem1a.swift
@@ -0,0 +1,8 @@
+//
+//  Problem1a.swift
+//  Substractive Cancelation Excercise 1
+//
+//  Created by PHYS 440 Marco on 1/25/24.
+//
+
+import Foundation
---
+++ Substractive Cancelation Excercise 1/Problem1b.swift
@@ -0,0 +1,8 @@
+//
+//  Problem1b.swift
+//  Substractive Cancelation Excercise 1
+//
+//  Created by PHYS 440 Marco on 1/25/24.
+//
+
+import Foundation
