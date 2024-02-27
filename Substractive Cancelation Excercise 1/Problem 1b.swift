05e106d68ebe874ea121d2773e2896d85a150698
GMarco2024 <mgonzalez11@hawk.iit.edu>
2024-02-26 17:09:23 -0800

    Changes were made.

--- Substractive Cancelation Excercise 1/Problem 1b.swift
+++ Substractive Cancelation Excercise 1/Problem 1b.swift
@@ -11,21 +11,23 @@
 import Foundation
 
 // Function to conduct the investigation for Problem 1b and return the results
+
 func conductInvestigation(n: Int) -> [String] {
     var results: [String] = []
 
-    // Iterate through values of n from 1 to the specified value
+// Iterate through values of n from 1 to the specified value
    
     for i in 1...n {
      
-        // Calculate c based on the provided formula c = 10^(-n)
+// Calculate c based on the provided formula c = 10^(-n)
        
         let c = pow(10, -Double(i))
 
        
-        // Append results for each iteration
+// Append results for each iteration
         
         results.append("For n = \(i), c = \(c)")
+        
     }
 
     return results
